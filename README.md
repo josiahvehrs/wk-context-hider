f8f78b6d7ccc5bc6c98b701a4349b35955567b1e
Josiah Vehrs <josiahvehrs@gmail.com>
2024-02-07 09:00:57 -0600

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # wk-context-hider
 
 This is a chrome extension that hides the english part of the context sentences on wanikani. 
+
+Clicking the japanese sentence will toggle visibility of the english sentence.
--- index.js
+++ index.js
@@ -15,5 +15,5 @@ const sentences = document.querySelectorAll(".context-sentences");
 sentences.forEach((node) => {
     node.children[0].style.cursor = "pointer";
     node.addEventListener('click', clickHandler);
-    node.children[1].style.display = none;
+    node.children[1].style.display = "none";
 });
